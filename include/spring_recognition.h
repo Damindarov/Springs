c491aac7c2711be52ebdbcedb663bc6a55481aea
Mikhail Terentev <termisha@mail.ru>
2023-08-05 22:06:03 +0300

    add: spring recognition, filtration_ang

--- include/spring_recognition.h
+++ include/spring_recognition.h
@@ -6,4 +6,10 @@
 #ifndef SPRINGS_SPRING_RECOGNITION_H
 #define SPRINGS_SPRING_RECOGNITION_H
 
+#include <tuple>
+
+#include "open3d/Open3D.h"
+
+std::tuple<Eigen::Vector3d, Eigen::Matrix3d> getTransformation(std::shared_ptr<open3d::geometry::PointCloud>);
+
 #endif //SPRINGS_SPRING_RECOGNITION_H
--- src/spring_recognition.cpp
+++ src/spring_recognition.cpp
@@ -1,3 +1,13 @@
 //
 // Created by PC on 8/3/2023.
 //
+
+#include "spring_recognition.h"
+
+std::tuple<Eigen::Vector3d, Eigen::Matrix3d> getTransformation(std::shared_ptr<open3d::geometry::PointCloud> cluster) {
+    auto obb = cluster->GetOrientedBoundingBox();
+    auto center = obb.center_;
+    auto R = obb.R_;
+
+    return std::make_tuple(center, R);
+}
